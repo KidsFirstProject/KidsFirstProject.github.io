285552c857a552273b7d97513fdb6d9acff703b4
Andrew Zhao <snareclaw@gmail.com>
2018-05-18 00:12:53 -0700

    Add routing

---
+++ kidsfirstproject/src/components/Donate.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => (
+  <span>DONATE</span>
+)
+
+export default Home;
---
+++ kidsfirstproject/src/components/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Layout } from 'antd';
+
+const Home = () => (
+  <span>Home</span>
+)
+
+export default Home;
