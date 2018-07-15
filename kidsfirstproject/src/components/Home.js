a25642bb6dcf3cdd4d6285c433f1c027d58dd34b
Andrew Zhao <snareclaw@gmail.com>
2018-07-15 01:02:31 -0700

    Add base carousel

--- kidsfirstproject/src/components/Home.js
+++ kidsfirstproject/src/components/Home.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Grid, Carousel } from 'antd';
 
 const Home = () => (
-  <span>Home</span>
+  <Carousel autoplay>
+    <div><h3>1</h3></div>
+    <div><h3>2</h3></div>
+    <div><h3>3</h3></div>
+    <div><h3>4</h3></div>
+  </Carousel>
+  
 )
 
 export default Home;
