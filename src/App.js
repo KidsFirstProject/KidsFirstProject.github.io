01643397af0c9c4c5948e79163e21a3a74c9e04f
Andrew Zhao <snareclaw@gmail.com>
2019-05-17 01:10:31 -0700

    fix(app): add fallback html routes for old links

--- src/App.js
+++ src/App.js
@@ -46,9 +46,26 @@ const App = () => (
         <Route exact path="/advocacy" component={AdvocacyPage} />
         <Route exact path="/blog" component={BlogList} />
         <Route exact path="/blog/:page" component={BlogList} />
-        <Route path="/blog/post/:id" component={BlogPage} />
-        <Route path="/donate" component={DonatePage} />
-        <Route path="/contact" component={ContactPage} />
+        <Route exact path="/blog/post/:id" component={BlogPage} />
+        <Route exact path="/donate" component={DonatePage} />
+        <Route exact path="/contact" component={ContactPage} />
+        {/* Fallback routes for old links */}
+        <Route exact path="/index.html" component={HomePage} />
+        <Route exact path="/about.html" component={AboutPage} />
+        <Route exact path="/team.html" component={TeamPage} />
+        <Route exact path="/programs/inshelter.html" component={InShelter} />
+        <Route
+          exact
+          path="/programs/scholarship.html"
+          component={Scholarship}
+        />
+        <Route exact path="/partners.html" component={PartnersPage} />
+        <Route exact path="/events.html" component={EventCalendar} />
+        <Route exact path="/volunteer.html" component={VolunteerPage} />
+        <Route exact path="/chapters.html" component={ChaptersPage} />
+        <Route exact path="/advocacy.html" component={AdvocacyPage} />
+        <Route exact path="/donate.html" component={DonatePage} />
+        <Route exact path="/contact.html" component={ContactPage} />
         <Route component={NotFound} />
       </Switch>
       <KFPFooter />
--- src/components/AdvocacyPage/index.js
+++ src/components/AdvocacyPage/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Col, Row, Image } from 'react-bootstrap';
-import advocacyPoster from '../../assets/images/programs/advocacy.png'
+import advocacyPoster from '../../assets/images/programs/advocacy.png';
 
 const AdvocacyPage = () => (
   <Container className="page-container">
