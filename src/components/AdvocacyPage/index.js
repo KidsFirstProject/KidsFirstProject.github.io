53638da1293853cbe806ff4905499b0e04ba6cfd
Andrew Zhao <snareclaw@gmail.com>
2019-05-17 00:56:10 -0700

    feat(advocacy): add poster

--- src/components/AdvocacyPage/index.js
+++ src/components/AdvocacyPage/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Container, Col, Row } from 'react-bootstrap';
+import { Container, Col, Row, Image } from 'react-bootstrap';
+import advocacyPoster from '../../assets/images/programs/advocacy.png'
 
 const AdvocacyPage = () => (
   <Container className="page-container">
     <h1>Summer Advocacy Fellowship</h1>
     <hr />
     <Row>
-      <Col />
+      <Col className="text-center">
+        <Image fluid src={advocacyPoster} />
+      </Col>
     </Row>
   </Container>
 );
