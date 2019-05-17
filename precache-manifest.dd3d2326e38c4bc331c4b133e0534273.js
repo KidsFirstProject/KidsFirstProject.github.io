9ebf4eaa316a8bdc2f83bb25879f5dc87645a6ca
Andrew Zhao <snareclaw@gmail.com>
2019-05-17 00:44:21 -0700

    Updates

---
+++ precache-manifest.dd3d2326e38c4bc331c4b133e0534273.js
@@ -0,0 +1,398 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f71c98f6811925e0dc8961e476ed4495",
+    "url": "/index.html"
+  },
+  {
+    "revision": "345bfd8809fafa4555a5",
+    "url": "/static/css/main.7309d1d5.chunk.css"
+  },
+  {
+    "revision": "e53d20516bdd03344b2d",
+    "url": "/static/js/2.cc463811.chunk.js"
+  },
+  {
+    "revision": "345bfd8809fafa4555a5",
+    "url": "/static/js/main.6f90e37e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8213742999c9858d75ae0b912149bdee",
+    "url": "/static/media/2015-08-03-01.82137429.jpg"
+  },
+  {
+    "revision": "7ec054426368ccf57753777d8044a8ae",
+    "url": "/static/media/2015-08-03-02.7ec05442.jpg"
+  },
+  {
+    "revision": "690e778f85416f92098c366330638716",
+    "url": "/static/media/2015-08-03-03.690e778f.jpg"
+  },
+  {
+    "revision": "9b818c64572e74bc0c9904723799f48b",
+    "url": "/static/media/2015-08-03-04.9b818c64.jpg"
+  },
+  {
+    "revision": "072469da5c8d999107a6a1b92e83921b",
+    "url": "/static/media/2015-08-05.072469da.jpg"
+  },
+  {
+    "revision": "617b06c28b1e999a17856aa2122f618b",
+    "url": "/static/media/2015-08-25.617b06c2.jpg"
+  },
+  {
+    "revision": "aa6d56290f423f8ff42fd4786b657683",
+    "url": "/static/media/2015-08-30-1.aa6d5629.jpg"
+  },
+  {
+    "revision": "22021efad1104e5468f4d19eefbc7bee",
+    "url": "/static/media/2015-08-30-2.22021efa.jpg"
+  },
+  {
+    "revision": "f8ace58ebb7202c46a7d2d528d04f231",
+    "url": "/static/media/2015-08-30-3.f8ace58e.jpg"
+  },
+  {
+    "revision": "56b44dc029c9b49f2b42d955b833d6a9",
+    "url": "/static/media/2015-08-30-4.56b44dc0.jpg"
+  },
+  {
+    "revision": "95a787abb33a8eee012a7fcf68569998",
+    "url": "/static/media/2015-08-30-5.95a787ab.jpg"
+  },
+  {
+    "revision": "1ceeb4a4fdb76eb164b45d4ae0206570",
+    "url": "/static/media/2015-09-01.1ceeb4a4.jpg"
+  },
+  {
+    "revision": "f99608677894c71b4f7acb33b17450ee",
+    "url": "/static/media/2015-09-18.f9960867.jpg"
+  },
+  {
+    "revision": "c5e9eb7af3519deb7d9c8db0c9c83d3b",
+    "url": "/static/media/2015-09-19-1.c5e9eb7a.jpg"
+  },
+  {
+    "revision": "21cdb6f47f204eccfa2e3d4990ee2ac9",
+    "url": "/static/media/2015-09-19-2.21cdb6f4.jpg"
+  },
+  {
+    "revision": "dfb0b996aadfdc21fd2894ca7340625f",
+    "url": "/static/media/2015-09-19-3.dfb0b996.jpg"
+  },
+  {
+    "revision": "a3a6216ce3e2cd6c2f7734f31fc6845e",
+    "url": "/static/media/2015-09-19-4.a3a6216c.jpg"
+  },
+  {
+    "revision": "87a7b208cad5d33ab73608d597066e7a",
+    "url": "/static/media/2015-09-19-5.87a7b208.jpg"
+  },
+  {
+    "revision": "aae2fbf62e97c8742774aef1c46c52e9",
+    "url": "/static/media/2015-11-01-1.aae2fbf6.jpg"
+  },
+  {
+    "revision": "8c186f0e38486e560565b95f488bde24",
+    "url": "/static/media/2015-11-01-2.8c186f0e.jpg"
+  },
+  {
+    "revision": "346916940e85f12207d97e122488215a",
+    "url": "/static/media/2015-11-01-3.34691694.jpg"
+  },
+  {
+    "revision": "014e371830117f2b3890c7ab59950999",
+    "url": "/static/media/2015-11-01-4.014e3718.jpg"
+  },
+  {
+    "revision": "3d24e6a3feae7436417b9b365b33fbe5",
+    "url": "/static/media/2015-11-01-5.3d24e6a3.jpg"
+  },
+  {
+    "revision": "57dffbc1adfadcd5aad75c40cfaf67e0",
+    "url": "/static/media/2015-11-01-6.57dffbc1.jpg"
+  },
+  {
+    "revision": "d5acf76bd19d6d3fba5026bf4430e53f",
+    "url": "/static/media/2015-11-01-7.d5acf76b.jpg"
+  },
+  {
+    "revision": "3963dbc61363a6e766a38e6ff2c2ff66",
+    "url": "/static/media/2015-11-01-8.3963dbc6.jpg"
+  },
+  {
+    "revision": "4f92ab39c9ddf3e844e942368d625777",
+    "url": "/static/media/2017-01-15.4f92ab39.png"
+  },
+  {
+    "revision": "b5f8e1ab779488525ffc6aee4aacec58",
+    "url": "/static/media/2017-01-17-1.b5f8e1ab.jpg"
+  },
+  {
+    "revision": "d1957e2a2d20c6efd6f8e025914a2e65",
+    "url": "/static/media/2017-01-17-2.d1957e2a.jpg"
+  },
+  {
+    "revision": "73d6b93cd6dce4afbad21d0b7ff06b4f",
+    "url": "/static/media/2017-04-18.73d6b93c.jpg"
+  },
+  {
+    "revision": "863327229e27607f7e8e5804b09438cc",
+    "url": "/static/media/2017-10-06.86332722.jpg"
+  },
+  {
+    "revision": "dded4bed42ede7105fed32ee208e1e64",
+    "url": "/static/media/2018-05-14.dded4bed.jpg"
+  },
+  {
+    "revision": "c21ddb87ce48a9c06215670a472dc80e",
+    "url": "/static/media/2018-05-18.c21ddb87.jpg"
+  },
+  {
+    "revision": "cb4aaea70f3e917360f084895abc18b7",
+    "url": "/static/media/An-Daae.cb4aaea7.jpg"
+  },
+  {
+    "revision": "45e4ab4d49187af934de4d232e5bebac",
+    "url": "/static/media/Asher-Malcom.45e4ab4d.jpg"
+  },
+  {
+    "revision": "a927e610524cc11df098e02523c9a27b",
+    "url": "/static/media/Bennette-Deyalo.a927e610.jpg"
+  },
+  {
+    "revision": "ce84ee03a0097ce50bab6cb4cf107c44",
+    "url": "/static/media/Carton-Abby.ce84ee03.jpg"
+  },
+  {
+    "revision": "156c06d177f15b20b6470e60cc18dabb",
+    "url": "/static/media/CentralCityConcern.156c06d1.png"
+  },
+  {
+    "revision": "747ffaa123b1ff60a10be686c2590a32",
+    "url": "/static/media/Chapman-Peyton.747ffaa1.jpg"
+  },
+  {
+    "revision": "ace907166ea5b5c2e614558aebe22610",
+    "url": "/static/media/Charitan-David.ace90716.jpg"
+  },
+  {
+    "revision": "ea84c4c5a52c47d74a35218358bd31ea",
+    "url": "/static/media/Eroh-Alli.ea84c4c5.jpg"
+  },
+  {
+    "revision": "6c7e575c8a6432f18c1c19551d821109",
+    "url": "/static/media/Fishback-Michaela.6c7e575c.jpg"
+  },
+  {
+    "revision": "8b8ee6db61cece8ef80a1fbd468634a6",
+    "url": "/static/media/Fred-Meyer-Non-profit.8b8ee6db.pdf"
+  },
+  {
+    "revision": "105ec80fe7e89d3df609ddb2ccc36fc2",
+    "url": "/static/media/GirlsScienceInteractive.105ec80f.png"
+  },
+  {
+    "revision": "456ffdaa04ec871f24636dcfaa29c9a0",
+    "url": "/static/media/HandsOn.456ffdaa.png"
+  },
+  {
+    "revision": "1028c362f652d61b97aa719511c70dd5",
+    "url": "/static/media/Harrison-Kimberly.1028c362.jpg"
+  },
+  {
+    "revision": "5b8b6f0dce257de2fd95f3789f464427",
+    "url": "/static/media/Home-Ashley.5b8b6f0d.jpg"
+  },
+  {
+    "revision": "a600b1d6fb7c1e0764ec9ac8bb0dcf8a",
+    "url": "/static/media/Jeffreys-Ella.a600b1d6.png"
+  },
+  {
+    "revision": "a783d2afe54276ada1a6483fe27a087f",
+    "url": "/static/media/Johnson-Mia.a783d2af.jpg"
+  },
+  {
+    "revision": "11136dceea95e16a7d6bdb577b049191",
+    "url": "/static/media/Kaliszewski-Gwen.11136dce.jpg"
+  },
+  {
+    "revision": "825cde930956ce767d34030788d6db73",
+    "url": "/static/media/Keepers-Claire.825cde93.jpg"
+  },
+  {
+    "revision": "b7c840880df53540901146cc36b8afa0",
+    "url": "/static/media/Lambert-Kate.b7c84088.jpg"
+  },
+  {
+    "revision": "eebd51ae4d9f0fec8cde75680a55a3ee",
+    "url": "/static/media/Lead-and-Inspire-PHFS-brochure.eebd51ae.pdf"
+  },
+  {
+    "revision": "a6e7e9e5d0e7e34b4551e7952e0479c5",
+    "url": "/static/media/Letter-from-Commissioner-Nick-Fish.a6e7e9e5.pdf"
+  },
+  {
+    "revision": "efc25ccb92d9f7aaf0aa7c44fb64aed5",
+    "url": "/static/media/Letter-from-Pat.efc25ccb.pdf"
+  },
+  {
+    "revision": "863327229e27607f7e8e5804b09438cc",
+    "url": "/static/media/Lin-Ashley.86332722.jpg"
+  },
+  {
+    "revision": "d195523b02cc69e1d582299da282569f",
+    "url": "/static/media/Ma-Emily.d195523b.png"
+  },
+  {
+    "revision": "df57904735a6b88af86fef19e78f8db4",
+    "url": "/static/media/Miller-Mirabella.df579047.jpg"
+  },
+  {
+    "revision": "5e190dedbe7acd6b68b988f0ae500805",
+    "url": "/static/media/Mrusko-Emily.5e190ded.jpg"
+  },
+  {
+    "revision": "4c6b880b32f8df1268b59cc9c6bb36aa",
+    "url": "/static/media/OMalley-Shannon.4c6b880b.jpg"
+  },
+  {
+    "revision": "bd10ce08d8f2a13bab0178db7c3cb7af",
+    "url": "/static/media/Peters-Simone.bd10ce08.jpg"
+  },
+  {
+    "revision": "6d2b9e50f11efd9d6872b2c6d2a7987f",
+    "url": "/static/media/Subramaniam-Brad.6d2b9e50.jpg"
+  },
+  {
+    "revision": "e44d0ff9e62a3eebbdfe9535ed47f4f2",
+    "url": "/static/media/Wilson-Riley.e44d0ff9.jpg"
+  },
+  {
+    "revision": "0f72fc38e7a311e143b0ead66aa58925",
+    "url": "/static/media/Winder-Julie.0f72fc38.jpg"
+  },
+  {
+    "revision": "f5a07fa53e5cb034f22e29f9f5f3a57a",
+    "url": "/static/media/Zhang-Sharon.f5a07fa5.jpg"
+  },
+  {
+    "revision": "ff1a70adb50e171bdf6627fb04197de0",
+    "url": "/static/media/Zhao-Alisha.ff1a70ad.jpg"
+  },
+  {
+    "revision": "ef3b412090100c51e66f14564767d3d8",
+    "url": "/static/media/ashoka.ef3b4120.png"
+  },
+  {
+    "revision": "d5f98959c0091fc354be2ff90ab462be",
+    "url": "/static/media/codered.d5f98959.jpg"
+  },
+  {
+    "revision": "4a8ecebe189f88aba0f85e4dbc53d86d",
+    "url": "/static/media/del-Pilar-Agudelo-Maria.4a8ecebe.jpg"
+  },
+  {
+    "revision": "51c7541a73a5c2709ab0d802985be749",
+    "url": "/static/media/festivalofChildrenFoundation.51c7541a.jpg"
+  },
+  {
+    "revision": "9966c391aa612ef3ae84726f2feb5ca4",
+    "url": "/static/media/fredmeyer.9966c391.png"
+  },
+  {
+    "revision": "f76d7587ae437736f1728b25607a4ce3",
+    "url": "/static/media/girls4change.f76d7587.png"
+  },
+  {
+    "revision": "04fc135c6f97bd3295424b78bbe02b36",
+    "url": "/static/media/hfh02.04fc135c.jpg"
+  },
+  {
+    "revision": "c5989879e172a5b0659791febcc833e1",
+    "url": "/static/media/hfh06.c5989879.jpg"
+  },
+  {
+    "revision": "435e7f2d6c67fa290ffe52efc95992cb",
+    "url": "/static/media/home-sweet-home-ministries.435e7f2d.png"
+  },
+  {
+    "revision": "4f7844bb7fc7066ca980417a507b482a",
+    "url": "/static/media/logo.4f7844bb.png"
+  },
+  {
+    "revision": "a8cabc388ed25afd9770942fd5f00c13",
+    "url": "/static/media/neighborhood-house-logo.a8cabc38.png"
+  },
+  {
+    "revision": "09cf8467cb0fccb44bb96bda44c0c638",
+    "url": "/static/media/noblehour-logo.09cf8467.png"
+  },
+  {
+    "revision": "04e7f3dc14f9c0dd34274ce5fd35ae7b",
+    "url": "/static/media/pic1.04e7f3dc.jpg"
+  },
+  {
+    "revision": "39054bfbbd8ea445ada92806c2acda29",
+    "url": "/static/media/pic2.39054bfb.jpg"
+  },
+  {
+    "revision": "f7dd3118e2f6b90681fba1694c3a7b08",
+    "url": "/static/media/pic3.f7dd3118.jpg"
+  },
+  {
+    "revision": "4af52696a7f6ad12b243a2e0a647b34b",
+    "url": "/static/media/pic4.4af52696.jpg"
+  },
+  {
+    "revision": "46341f5d8c43f5651f620557eba0251f",
+    "url": "/static/media/prudential.46341f5d.png"
+  },
+  {
+    "revision": "13ddbc598e58dd24d38ef2e46335f379",
+    "url": "/static/media/psu.13ddbc59.png"
+  },
+  {
+    "revision": "b20757073d5ba5aa0ca5cb14f7621901",
+    "url": "/static/media/rescue-team-kenya.b2075707.png"
+  },
+  {
+    "revision": "6ec542fe77a01d70df8fa55fd2e0b420",
+    "url": "/static/media/shelter001.6ec542fe.jpg"
+  },
+  {
+    "revision": "c30f90c328c1dec440b9898df9baf5c3",
+    "url": "/static/media/shelter002.c30f90c3.jpg"
+  },
+  {
+    "revision": "336375fe3955be03f9d01a5c9aa8d6bb",
+    "url": "/static/media/shelter003.336375fe.jpg"
+  },
+  {
+    "revision": "6b04b652a2fb3970db94154c17a7a890",
+    "url": "/static/media/shelter005.6b04b652.jpg"
+  },
+  {
+    "revision": "49c90ec3fb7d59f5b06a34a0a7d93057",
+    "url": "/static/media/shelter010.49c90ec3.jpg"
+  },
+  {
+    "revision": "08f395b974796e931dfe278b50a5c8bc",
+    "url": "/static/media/shelter016.08f395b9.jpg"
+  },
+  {
+    "revision": "53c94b84ee73eed40e1b6612ff620005",
+    "url": "/static/media/shelter018.53c94b84.jpg"
+  },
+  {
+    "revision": "286d1c68b2f12f3bf05433b47e31fc66",
+    "url": "/static/media/shelter020.286d1c68.jpg"
+  },
+  {
+    "revision": "9597783fe1c50a788d1852d76e62c689",
+    "url": "/static/media/threedotdash.9597783f.png"
+  }
+]);
--- src/utils/imageImport.js
+++
@@ -1,9 +0,0 @@
-const importAll = r => {
-  const images = {};
-  r.keys().forEach(item => {
-    images[item.replace('./', '')] = r(item);
-  });
-  return images;
-};
-
-export default importAll;
